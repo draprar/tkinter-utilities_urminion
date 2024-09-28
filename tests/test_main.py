b003aa5b462b841d83d520d7069cb10945d3b845
Walery <waleron@gmail.com>
2024-09-28 18:43:17 +0200

    test main & configure GitHub Actions for CI/CD

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 import sys
 import os
+from unittest.mock import patch
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main import Application
@@ -8,10 +9,12 @@ from main import Application
 
 class TestMain:
     @pytest.fixture
-    def app(self):
+    def app(self, mocker):
         """
         Create Application instance.
+        Mock Tkinter mainloop and other methods.
         """
+        mocker.patch('tkinter.Tk.__init__', return_value=None)
         return Application()
 
     def test_app_initialization(self, app):
